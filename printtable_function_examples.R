717b8bf75be77646833183105b390a448a3e5013
Eric Parajon <ericparajon@gmail.com>
2025-01-02 17:29:38 -0500

    added example 2

--- printtable_function_examples.R
+++ printtable_function_examples.R
@@ -10,3 +10,12 @@ table1_df <- mtcars[,c("mpg","wt") ]
 
 #exporting toy data to LaTeX
 printtable(table1_df,"Comparing car mpg and weight")
+
+#2.
+#Generating toy data from swiss
+
+#selecting the 10 provinces with the highest % Catholic population.
+table2_df<-head(swiss[rev(order(swiss$Catholic)),],10)
+
+#exporting toy data to LaTeX
+printtable(table2_df,"Fertility measure and socio-economic indicators for the French-speaking provinces of Switzerland in 1888 with the highest % Catholic")
